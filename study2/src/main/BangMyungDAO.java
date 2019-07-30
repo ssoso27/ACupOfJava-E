447c4e42c658af1e984b786b1a109a583ce35c1b
ssoso27 <sohi8290@gmail.com>
2019-07-30 16:21:24 +0900

    clean workspace

--- study2/src/main/BangMyungDAO.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-import java.util.List;
-
-public interface BangMyungDAO {
-	public void add ( BangMyungVO vo ) throws Exception;
-	public List<BangMyungVO> findAll() throws Exception;
-}
